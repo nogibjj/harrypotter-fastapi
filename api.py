52a46798580887348bcdca0abb320c2cd9249b36
Ultraviolet0909 <65413798+Ultraviolet0909@users.noreply.github.com>
2022-12-06 05:51:53 +0000

    Add GET action for most common words

--- api.py
+++ api.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-import gunicorn
-import uvicorn
-import uuid 
-import io 
 from pydantic import BaseModel
 from generate import generate_corpus, finish_sentence
+from dataanalysis import get_freq
 
 app = FastAPI()
 
@@ -16,26 +13,36 @@ class Input(BaseModel):
 
 @app.get("/")
 def home():
-    return {'Message': 'Welcome to the Harry Potter Text Generator API!'}
+    return {'message': 'Welcome to the Harry Potter Text Generator API!'}
 
+# Show the content of the corpes book in the list of 7 books
 @app.get("/list-of-books/{bookid}")
 def list_of_books(bookid: int): 
     if bookid >= 1 and bookid <= 7:
         text_file = open('Data/Book' + str(bookid) + '.txt', "r", encoding="utf-8")
-        data = text_file.read().strip()
+        data = text_file.read()
         text_file.close()
+        data = data.replace("\n", "").replace("\r", "")
         return {"book" : data}
     else: 
         return {"book": "Please enter a valid book id from 1 - 7."}
-
 
 @app.get("/generate-new-text/{id}")
 def generate(id: int): 
     return {'Generation result': id} 
 
+# Calculate the frequency of top 20 most common words for the book with input id
+@app.get("/frequency-of-top-20-most-common-words/{bookid}")
+def common_words(bookid: int): 
+    if bookid >= 1 and bookid <= 7:
+        result = get_freq(bookid)
+        return {"data": result}
+    else: 
+        return {"message": "Please enter a valid book id from 1 - 7."}
 
+# Generate harry potter styled text based on user input
 @app.post("/inputs/")
 def user_input(input: Input): 
     corpus = generate_corpus(input.book_num)
     ans = finish_sentence(input.sentence, input.n, corpus, input.text_length)
-    return {"status": ans}
+    return {"message": ans}
